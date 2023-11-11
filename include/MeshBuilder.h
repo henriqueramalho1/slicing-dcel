7cedea69ad9c80570c5eee1b37556746eb05aa8a
Henrique <henrique.ramalho2002@gmail.com>
2023-11-10 23:02:35 -0300

    Updating gitigonre

--- include/MeshBuilder.h
+++ include/MeshBuilder.h
@@ -1,23 +1,18 @@
 #pragma once
 #include "Mesh_DCEL.h"
 #include <CGAL/Surface_mesh.h>
-#include <CGAL/Polyhedron_3.h>
-#include <CGAL/Simple_cartesian.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/IO/io.h>
 #include <CGAL/HalfedgeDS_default.h>
 #include <CGAL/Polygon_mesh_processing/IO/polygon_mesh_io.h>
 
-//typedef CGAL::Simple_cartesian<double>     Kernel;
 typedef CGAL::Exact_predicates_inexact_constructions_kernel  Kernel;
 typedef CGAL::Surface_mesh<Kernel::Point_3>                      SurfaceMesh;
-typedef CGAL::Polyhedron_3<Kernel>                  Polyhedron;
 
 class MeshBuilder
 {
 private:
 	SurfaceMesh mesh;
-	Polyhedron mesh2;
 	Mesh_DCEL* dcel;
 
 public:
